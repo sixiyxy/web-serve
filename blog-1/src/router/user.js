46129b39f2123827a56d5226c29320cfe8476dd8
sixiyxy <771702500@qq.com>
2021-12-07 17:29:41 +0800

    6-10

--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,19 +1,42 @@
-const { loginCheck } = require('../controller/user')
+const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+
 
 const handleUserRouter = (req, res) => {
     const method = req.method
 
-    if (method === 'POST' && req.path === '/api/user/login') {
-        const { username, password} = req.body;
-        const result = loginCheck(username, password)
+    if (method === 'GET' && req.path === '/api/user/login') {
+        //const { username, password} = req.body;
+        const { username, password } = req.query;
+        const result = login(username, password);
         return result.then(data => {
             if ( data.username ) {
+                //操作cookie
+                //res.setHeader('Set-Cookie',`username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
+
+                //设置session
+                req.session.username = data.username 
+                req.session.realname = data.realname
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败')
         })
         
+    }
+
+    //登录验证测试
+    if (method === 'GET' && req.path === '/api/user/login-test') {
+        if(req.session.username) {
+            return Promise.resolve(
+                new SuccessModel({
+                    session: req.session
+                })
+            )
+        }
+        return Promise.resolve(
+            new ErrorModel('尚未登录')
+        )
     }
 }
 module.exports = handleUserRouter
