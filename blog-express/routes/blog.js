097d2eda0686a8623d65abf4a9efd3bab491d375
sixiyxy <771702500@qq.com>
2021-12-10 14:53:34 +0800

    9-10

---
+++ blog-express/routes/blog.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+router.get('/list', function(req, res, next) {
+    let author = req.query.author || ''
+    const keyword = req.query.keyword || ''
+
+    // if (req.query.isadmin) {
+    //     console.log('is admin')
+    //     // 管理员界面
+    //     if (req.session.username == null) {
+    //         console.error('is admin, but no login')
+    //         // 未登录
+    //         res.json(
+    //             new ErrorModel('未登录')
+    //         )
+    //         return
+    //     }
+    //     // 强制查询自己的博客
+    //     author = req.session.username
+    // }
+
+    const result = getList(author, keyword)
+    return result.then(listData => {
+        res.json(
+            new SuccessModel(listData)
+        )
+    })
+});
+
+module.exports = router;
